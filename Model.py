f89953d35916a2afd9f3fb0a45e792f5bf5fdb89
Chandler Potter <ChandlerPotter@MacBook-Pro.local>
2019-01-21 15:20:59 -0800

    Created a Model class

---
+++ Model.py
@@ -0,0 +1,14 @@
+#Chandler Potter 1/21/19 CIS 422
+
+class Model:
+    def __init__(self):
+        open('data.txt') #file with all of the saved data
+
+    def add_event(month, day):
+        return
+
+    def delete_event(month, day):
+        return
+
+    def get_event(month, day):
+        return
