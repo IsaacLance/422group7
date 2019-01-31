8d3d80c319ee9ad36d9f381f2088a67685ff7481
ilance <ilance@uoregon.edu>
2019-01-31 14:44:52 -0800

    Working on getting the model to a working state

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
 #Chandler Potter 1/21/19 CIS 422
 import shelve
+import Event
 
 class CalendarModel:
     def __init__(self, month, year):
@@ -15,6 +16,7 @@ class CalendarModel:
         print("Month: " +str(self.month))
         self.s = shelve.open("data", writeback = True)
         if not('events' in self.s):
+            print("No events key found in shelve. Created new events list."
             self.s['events'] = []
         return
 
@@ -22,7 +24,7 @@ class CalendarModel:
         print("Model exit")
         return
     
-    #Button responses
+    #Button responses/ setters
     def next_month(self):
         self.month += 1
         if self.month == 13:
@@ -39,14 +41,37 @@ class CalendarModel:
         return
         
     def add_event(self, title, start, end): #adds event at specified date (month, day)
+        print("Added event")
         print(title)
         print(start)
         print(end)
+        self.s['events'].append(Event(title, start, end))
 
         return
+    #Getters
+    def get_year(self):
+        #get the current year
+        #print(self.s[event]['year'])
+        return self.year
+
+    def get_month(self):
+        #get the current month
+        #print(self.s[event]['month'])
+        return self.month   
+    #Helpers
+    '''
+    These don't work yet
+    def search_event(self, event)
+        #The format of the event must be "a-b-c"
+        #here a means month,b means day, c means year
+        if event in self.s['events']:
+            return True
+        else:
+            return False    
 
     def delete_event(month, day): #deletes an event at specified date (month, day)
         return
-
+    
     def get_event(month, day): #retrieves an event at specified date (month, day) and presents that to the user in the ViewController
         return
+    '''
